fe7b86cd25adde53eca479896d781e8971d5833f
framirez-boop <f.ramirez@mcaabhm.org>
2026-05-12 15:18:37 -0500

    about me and js update

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let deck = [};
 let playerHand = [];
 let cpuHand = [];
+let discardPile = [];
+
+function startGame(){
+deck = [];
+colors.forEach(color)
+values.forEach(value)
+deck.push({ color, value})
+ if (value !== "0") deck.push({ color, value });
+}]
+
+}
