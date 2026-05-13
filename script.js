ab28148465352d76869a7f854d9b10aa593bd78a
framirez-boop <f.ramirez@mcaabhm.org>
2026-05-13 14:49:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 let deck = [};
 let playerHand = [];
-let cpuHand = [];
+let computer = [];
 let discardPile = [];
 
 function startGame(){
